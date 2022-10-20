7db7028f63dc33a277c7440dacbcb845668e35a3
Mami. N <mamypoco@gmail.com>
2022-10-20 09:05:25 -0700

    fixed fontawesome icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
    return (
